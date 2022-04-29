f13725a96a5d4b542c9bdf68cf5ea0c29c47ca3e
Dmitriy <dim94@mail.ru>
2022-04-29 14:04:55 +0300

    fix controller

--- db/migrations/20220428134538-create-user.js
+++ db/migrations/20220428134538-create-user.js
@@ -1,4 +1,3 @@
-'use strict';
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.createTable('Users', {
@@ -11,50 +10,50 @@ module.exports = {
       userName: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique:true,
+        unique: true,
         len: [4, 20],
         is: /^[a-z]+([-_]?[a-z0-9]+){0,2}$/i,
-        //Начинается и кончается на букву/цифру и содержит не более двух "_"/"-" и не подряд.
+        // Начинается и кончается на букву/цифру и содержит не более двух "_"/"-" и не подряд.
       },
       email: {
         type: Sequelize.STRING,
         allowNull: false,
-        unique:true,
+        unique: true,
         len: [3, 30],
         is: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
       },
       password: {
         type: Sequelize.TEXT,
         allowNull: false,
-        len: [6, 20]
+        len: [6, 20],
       },
       image: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       phone: {
         type: Sequelize.TEXT,
-        unique:true
+        unique: true,
       },
       about_user: {
         type: Sequelize.TEXT,
       },
       role: {
-        type: Sequelize.TEXT
+        type: Sequelize.TEXT,
       },
       isBanned: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
   async down(queryInterface, Sequelize) {
     await queryInterface.dropTable('Users');
-  }
+  },
 };
