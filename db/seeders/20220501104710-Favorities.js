179c1b5a119d15d4149824c4660763f40dcaf83f
airgirl2305 <kheda.askhabova@gmail.com>
2022-05-11 17:08:05 +0300

    seeded favorites

--- db/seeders/20220501104710-Favorities.js
+++ db/seeders/20220501104710-Favorities.js
@@ -1,11 +1,54 @@
 module.exports = {
   async up(queryInterface) {
     await queryInterface.bulkInsert('Favorites', [{
-      userId: 1,
-      advertisementId: 1,
+      userId: 5,
+      advertisementId: 19,
       createdAt: new Date(),
       updatedAt: new Date(),
-    }], {});
+    },
+    {
+      userId: 5,
+      advertisementId: 22,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      userId: 4,
+      advertisementId: 7,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      userId: 3,
+      advertisementId: 5,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      userId: 3,
+      advertisementId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      userId: 6,
+      advertisementId: 11,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      userId: 2,
+      advertisementId: 3,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    {
+      userId: 2,
+      advertisementId: 16,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    },
+    ], {});
   },
 
   async down(queryInterface) {
